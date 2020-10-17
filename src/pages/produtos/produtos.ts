dfede73d43ff5325b7881c75080a9d98853f21a7
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2020-10-16 22:07:19 -0300

    Infinite Scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -18,7 +18,8 @@ import { ProdutoService } from '../../services/domain/produto.service';
 })
 export class ProdutosPage {
 
-  items : ProdutoDTO[];
+  items : ProdutoDTO[] = [];
+  page : number = 0;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public produtoService: ProdutoService, public loadController: LoadingController) {
   }
@@ -30,18 +31,20 @@ export class ProdutosPage {
   loadData(){
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id).subscribe(response => {
-      this.items = response['content'];
+    this.produtoService.findByCategoria(categoria_id, this.page, 10).subscribe(response => {
+      let start = this.items.length;
+      this.items = this.items.concat(response['content']);
+      let end = this.items.length - 1;
       loader.dismiss();
-      this.loadImageUrls();
+      this.loadImageUrls(start, end);
     },
     error => {
       loader.dismiss();
     });
   }
 
-  loadImageUrls(){
-    for(var i=0; i<this.items.length; i++){
+  loadImageUrls(start : number, end : number){
+    for(var i=start; i<=end; i++){
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id).subscribe(response => {
         item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
@@ -64,9 +67,19 @@ export class ProdutosPage {
   }
 
   doRefresh(refresher) {
+    this.page = 0;
+    this.items = [];
     this.loadData();
      setTimeout(() => {
       refresher.complete();
+    }, 2000);
+  }
+
+  doInfinite(infiniteScroll){
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+     infiniteScroll.complete();
     }, 2000);
   }
 }
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -14,8 +14,8 @@ export class ProdutoService {
     return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
   }
 
-  findByCategoria(categoria_id : string) {
-    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categories=${categoria_id}`);
+  findByCategoria(categoria_id : string, page : number = 0, linesPerPage : number = 24) {
+    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categories=${categoria_id}&page=${page}&¨linesPerPage=${linesPerPage}`);
   }
 
   getSmallImageFromBucket(id : string) : Observable<any>{

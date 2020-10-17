436c37add8000b99398c78c0854ca2f79a402d59
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2020-10-16 21:07:01 -0300

    Informing registered order to the user

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -26,6 +26,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public cartService: CartService, public clienteService: ClienteService, public pedidoService: PedidoService) {
   
@@ -56,7 +57,8 @@ export class OrderConfirmationPage {
   checkout(){
     this.pedidoService.insert(this.pedido).subscribe(response =>{
       this.cartService.createOrClearCart();
-      console.log(response.headers.get('location'));
+      this.codPedido = this.extractId(response.headers.get('location'));
+      console.log(this.codPedido);
     },
     error => {
       if (error.status == 403){
@@ -67,5 +69,14 @@ export class OrderConfirmationPage {
 
   back(){
     this.navCtrl.setRoot('CartPage');
+  }
+
+  private extractId(location: string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position+1,location.length);
+  }
+
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
   }
 }
